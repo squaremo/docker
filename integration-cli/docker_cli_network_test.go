29521c099fc640e5bc05c4a5bf1e51658698d0a7
Madhu Venugopal <madhu@docker.com>
2015-05-19 23:17:41 -0700

    User-specified --net option
    Introducing a new mode called "user-defined" network mode wherein the
    user can specify a network with a name that is created using "docker
    network" commands.
    Also passing the container labels as generic options for the drivers to
    make use of.
    
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- integration-cli/docker_cli_network_test.go
+++ integration-cli/docker_cli_network_test.go
@@ -50,3 +50,52 @@ func (s *DockerSuite) TestDockerNetworkCreateDelete(c *check.C) {
 		c.Fatalf("Network test is not removed")
 	}
 }
+
+func (s *DockerSuite) TestDockerNetworkValidRunContainer(c *check.C) {
+	runCmd := exec.Command(dockerBinary, "network", "create", "udn")
+	out, _, _, err := runCommandWithStdoutStderr(runCmd)
+	if err != nil {
+		c.Fatal(out, err)
+	}
+
+	out, _, err = runCommandWithOutput(exec.Command(dockerBinary, "run", "-d", "--net", "udn", "--name", "host_container", "busybox", "top"))
+	if err != nil {
+		c.Fatal(err, out)
+	}
+
+	out, _, err = runCommandWithOutput(exec.Command(dockerBinary, "rm", "-f", "host_container"))
+	if err != nil {
+		c.Fatal(err, out)
+	}
+
+	runCmd = exec.Command(dockerBinary, "network", "rm", "udn")
+	out, _, _, err = runCommandWithStdoutStderr(runCmd)
+	if err != nil {
+		c.Fatal(out, err)
+	}
+}
+
+func (s *DockerSuite) TestDockerNetworkInvalidRunContainer(c *check.C) {
+	_, _, err := runCommandWithOutput(exec.Command(dockerBinary, "run", "-d", "--net", "invalid", "--name", "host_container", "busybox", "top"))
+	if err == nil {
+		c.Fatal("Invalid user-defined network should fail container creation")
+	}
+}
+
+func (s *DockerSuite) TestDockerNetworkLinksUserNetwork(c *check.C) {
+	runCmd := exec.Command(dockerBinary, "network", "create", "userdefined")
+	out, _, _, err := runCommandWithStdoutStderr(runCmd)
+	if err != nil {
+		c.Fatal(out, err)
+	}
+
+	out, _, err = runCommandWithOutput(exec.Command(dockerBinary, "run", "-d", "--net", "userdefined", "--name", "host_container", "busybox", "top"))
+	if err != nil {
+		c.Fatal(err, out)
+	}
+
+	out, _, err = runCommandWithOutput(exec.Command(dockerBinary, "run", "--name", "should_fail", "--link", "host_container:tester", "busybox", "true"))
+	if err == nil || !strings.Contains(out, "User-Defined --netoption can't be used with links") {
+		c.Fatalf("Running container linking to a container with an user-defined --net should have failed: %s", out)
+	}
+}
